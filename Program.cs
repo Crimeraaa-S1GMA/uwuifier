1c2b885ba8ebab00d71e897627d832a7f562f06b
Crimeraaa-S1GMA <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-04-21 23:12:29 +0200

    Upload project files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿global using TextCopy;
+
+UwUifier.Uwuifier uwuifier = new UwUifier.Uwuifier();
+
+while(true)
+{
+    Console.Write("Provide text to uwuify: ");
+    var input = Console.ReadLine();
+    if (input != null)
+    {
+        string uwuResult = uwuifier.Uwuify(input);
+        ClipboardService.SetText(uwuResult);
+        Console.WriteLine("Result: "+ uwuResult + "\n\nCopied result to clipboard!\nPress any key to continue with new output...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ Uwuifier.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwUifier
+{
+    internal class Uwuifier
+    {
+        public string Uwuify(string input)
+        {
+            List<string> outputs = QuickSplit(input);
+            
+            outputs = UwuifyLettersInStrList(outputs);
+            outputs = AddRandomEmoticons(outputs);
+            
+            return ConnectStrList(outputs);
+        }
+
+        public string ConnectStrList(List<string> input)
+        {
+            string output = string.Empty;
+
+            foreach(string str in input)
+            {
+                output += str + " ";
+            }
+
+            output = output.Trim();
+
+            return output;
+        }
+
+        /// <summary>
+        /// Quickly split text to use it with the uwuify methods
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public List<string> QuickSplit(string input)
+        {
+            return input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        public List<string> AddRandomEmoticons(List<string> input)
+        {
+            Random random = new Random();
+
+            string[] emoticons =
+            {
+                "OwO",
+                "UwU",
+                "QwQ",
+                "^w^",
+                ":3",
+                "u3u",
+                "(～￣▽￣)～",
+                "ヾ(≧▽≦*)o",
+                "(❁´◡`❁)"
+            };
+
+            List<string> output = input;
+
+            output.Add(emoticons[random.Next(0, emoticons.Length)]);
+
+            return output;
+        }
+
+        public List<string> UwuifyLettersInStrList(List<string> input)
+        {
+            List<string> output = new List<string>();
+
+            foreach(string str in input)
+            {
+                output.Add(UwuifyLettersInStr(str));
+            }
+
+            return output;
+        }
+
+        public string UwuifyLettersInStr(string input)
+        {
+            return input.Replace('r', 'w').Replace('l', 'w').Replace('R', 'W').Replace('L', 'W');
+        }
+    }
+}
